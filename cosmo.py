e2720b6896bd4583442830c2022aa8feae2764a7
lhlam0 <1155143373@link.cuhk.edu.hk>
2023-11-20 01:00:23 +0800

    added cosmo.py
    llava does not give satisfactory output yet

---
+++ cosmo.py
@@ -0,0 +1,399 @@
+import argparse
+import os
+import sys
+import json
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer, BitsAndBytesConfig, CLIPImageProcessor
+
+
+from model.LISA import LISAForCausalLM
+from model.llava import conversation as conversation_lib
+from model.llava.conversation import conv_templates, SeparatorStyle
+from model.llava.model.builder import load_pretrained_model
+from model.llava.utils import disable_torch_init
+from model.llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from model.llava.mm_utils import process_images, tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+from model.segment_anything.utils.transforms import ResizeLongestSide
+from utils.utils import (DEFAULT_IM_END_TOKEN, DEFAULT_IM_START_TOKEN,
+                         DEFAULT_IMAGE_TOKEN, IMAGE_TOKEN_INDEX)
+from paddleocr import PaddleOCR,draw_ocr
+from PIL import Image
+
+import requests
+from io import BytesIO
+from transformers import TextStreamer
+
+def parse_args(args):
+    # lisa args
+    parser = argparse.ArgumentParser(description="COSMO chat")
+    parser.add_argument("--version", default="xinlai/LISA-13B-llama2-v1")
+    parser.add_argument("--vis_save_path", default="./vis_output", type=str)
+    parser.add_argument(
+        "--precision",
+        default="bf16",
+        type=str,
+        choices=["fp32", "bf16", "fp16"],
+        help="precision for inference",
+    )
+    parser.add_argument("--image_size", default=1024, type=int, help="image size")
+    parser.add_argument("--model_max_length", default=512, type=int)
+    parser.add_argument("--lora_r", default=8, type=int)
+    parser.add_argument(
+        "--vision-tower", default="openai/clip-vit-large-patch14", type=str
+    )
+    parser.add_argument("--local-rank", default=0, type=int, help="node rank")
+    parser.add_argument("--load_in_8bit", action="store_true", default=False)
+    parser.add_argument("--load_in_4bit", action="store_true", default=False)
+    parser.add_argument("--use_mm_start_end", action="store_true", default=True)
+    parser.add_argument(
+        "--conv_type",
+        default="llava_v1",
+        type=str,
+        choices=["llava_v1", "llava_llama_2"],
+    )
+    
+    # llava args
+    parser.add_argument("--model-path", type=str, default="facebook/opt-350m")
+    parser.add_argument("--model-base", type=str, default=None)
+    # parse image path from ocr result
+    # parser.add_argument("--image-file", type=str, required=True)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--conv-mode", type=str, default=None)
+    parser.add_argument("--temperature", type=float, default=0.2)
+    parser.add_argument("--max-new-tokens", type=int, default=512)
+    parser.add_argument("--load-8bit", action="store_true")
+    parser.add_argument("--load-4bit", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--image-aspect-ratio", type=str, default='pad')
+    return parser.parse_args(args)
+
+
+def preprocess(
+    x,
+    pixel_mean=torch.Tensor([123.675, 116.28, 103.53]).view(-1, 1, 1),
+    pixel_std=torch.Tensor([58.395, 57.12, 57.375]).view(-1, 1, 1),
+    img_size=1024,
+) -> torch.Tensor:
+    """Normalize pixel values and pad to a square input."""
+    # Normalize colors
+    x = (x - pixel_mean) / pixel_std
+    # Pad
+    h, w = x.shape[-2:]
+    padh = img_size - h
+    padw = img_size - w
+    x = F.pad(x, (0, padw, 0, padh))
+    return x
+
+
+def main(args):
+    args = parse_args(args)
+    os.makedirs(args.vis_save_path, exist_ok=True)
+
+    ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
+    # Create model
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.version,
+        cache_dir=None,
+        model_max_length=args.model_max_length,
+        padding_side="right",
+        use_fast=False,
+    )
+    tokenizer.pad_token = tokenizer.unk_token
+    args.seg_token_idx = tokenizer("[SEG]", add_special_tokens=False).input_ids[0]
+
+
+    torch_dtype = torch.float32
+    if args.precision == "bf16":
+        torch_dtype = torch.bfloat16
+    elif args.precision == "fp16":
+        torch_dtype = torch.half
+
+    kwargs = {"torch_dtype": torch_dtype}
+    if args.load_in_4bit:
+        kwargs.update(
+            {
+                "torch_dtype": torch.half,
+                "load_in_4bit": True,
+                "quantization_config": BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    bnb_4bit_compute_dtype=torch.float16,
+                    bnb_4bit_use_double_quant=True,
+                    bnb_4bit_quant_type="nf4",
+                    llm_int8_skip_modules=["visual_model"],
+                ),
+            }
+        )
+    elif args.load_in_8bit:
+        kwargs.update(
+            {
+                "torch_dtype": torch.half,
+                "quantization_config": BitsAndBytesConfig(
+                    llm_int8_skip_modules=["visual_model"],
+                    load_in_8bit=True,
+                ),
+            }
+        )
+
+    model = LISAForCausalLM.from_pretrained(
+        args.version, low_cpu_mem_usage=True, vision_tower=args.vision_tower, seg_token_idx=args.seg_token_idx, **kwargs
+    )
+
+    model.config.eos_token_id = tokenizer.eos_token_id
+    model.config.bos_token_id = tokenizer.bos_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
+
+    model.get_model().initialize_vision_modules(model.get_model().config)
+    vision_tower = model.get_model().get_vision_tower()
+    vision_tower.to(dtype=torch_dtype)
+
+    if args.precision == "bf16":
+        model = model.bfloat16().cuda()
+    elif (
+        args.precision == "fp16" and (not args.load_in_4bit) and (not args.load_in_8bit)
+    ):
+        vision_tower = model.get_model().get_vision_tower()
+        model.model.vision_tower = None
+        import deepspeed
+
+        model_engine = deepspeed.init_inference(
+            model=model,
+            dtype=torch.half,
+            replace_with_kernel_inject=True,
+            replace_method="auto",
+        )
+        model = model_engine.module
+        model.model.vision_tower = vision_tower.half().cuda()
+    elif args.precision == "fp32":
+        model = model.float().cuda()
+
+    vision_tower = model.get_model().get_vision_tower()
+    vision_tower.to(device=args.local_rank)
+
+    clip_image_processor = CLIPImageProcessor.from_pretrained(model.config.vision_tower)
+    transform = ResizeLongestSide(args.image_size)
+
+    model.eval()
+
+    while True:
+        conv = conversation_lib.conv_templates[args.conv_type].copy()
+        conv.messages = []
+
+        prompt = input("Please input your prompt: ")
+        llava_prompt = prompt
+        prompt = "Segment " + prompt
+        
+        prompt = DEFAULT_IMAGE_TOKEN + "\n" + prompt
+        if args.use_mm_start_end:
+            replace_token = (
+                DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+            )
+            prompt = prompt.replace(DEFAULT_IMAGE_TOKEN, replace_token)
+
+        conv.append_message(conv.roles[0], prompt)
+        conv.append_message(conv.roles[1], "")
+        prompt = conv.get_prompt()
+
+        image_path = input("Please input the image path: ")
+        if not os.path.exists(image_path):
+            print("File not found in {}".format(image_path))
+            continue
+
+        image_np = cv2.imread(image_path)
+        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        original_size_list = [image_np.shape[:2]]
+
+        image_clip = (
+            clip_image_processor.preprocess(image_np, return_tensors="pt")[
+                "pixel_values"
+            ][0]
+            .unsqueeze(0)
+            .cuda()
+        )
+        if args.precision == "bf16":
+            image_clip = image_clip.bfloat16()
+        elif args.precision == "fp16":
+            image_clip = image_clip.half()
+        else:
+            image_clip = image_clip.float()
+
+        image = transform.apply_image(image_np)
+        resize_list = [image.shape[:2]]
+
+        image = (
+            preprocess(torch.from_numpy(image).permute(2, 0, 1).contiguous())
+            .unsqueeze(0)
+            .cuda()
+        )
+        if args.precision == "bf16":
+            image = image.bfloat16()
+        elif args.precision == "fp16":
+            image = image.half()
+        else:
+            image = image.float()
+
+        input_ids = tokenizer_image_token(prompt, tokenizer, return_tensors="pt")
+        input_ids = input_ids.unsqueeze(0).cuda()
+
+        output_ids, pred_masks = model.evaluate(
+            image_clip,
+            image,
+            input_ids,
+            resize_list,
+            original_size_list,
+            max_new_tokens=512,
+            tokenizer=tokenizer,
+        )
+        output_ids = output_ids[0][output_ids[0] != IMAGE_TOKEN_INDEX]
+
+        text_output = tokenizer.decode(output_ids, skip_special_tokens=False)
+        text_output = text_output.replace("\n", "").replace("  ", " ")
+        print("text_output: ", text_output)
+        with open("./text.json", "w") as f:
+            json.dump(text_output, f)
+        
+        for i, pred_mask in enumerate(pred_masks):
+            if pred_mask.shape[0] == 0:
+                continue
+
+            pred_mask = pred_mask.detach().cpu().numpy()[0]
+            pred_mask = pred_mask > 0
+
+            image_np_cropped = image_np.copy()
+            image_np_cropped[~pred_mask] = 0
+
+            # Find the bounding box coordinates of the cropped region
+            rows = np.any(pred_mask, axis=1)
+            cols = np.any(pred_mask, axis=0)
+            ymin, ymax = np.where(rows)[0][[0, -1]]
+            xmin, xmax = np.where(cols)[0][[0, -1]]
+
+            # Crop the image
+            image_np_cropped = image_np_cropped[ymin:ymax+1, xmin:xmax+1]
+
+            image_np_cropped = cv2.cvtColor(image_np_cropped, cv2.COLOR_RGB2BGR)
+
+            save_path = "{}/{}_cropped_{}.jpg".format(
+                args.vis_save_path, image_path.split("/")[-1].split(".")[0], i
+            )
+            cv2.imwrite(save_path, image_np_cropped)
+            print("{} has been saved.".format(save_path))
+
+            img_path = save_path
+
+            result = ocr.ocr(img_path, cls=True)
+            #print("result", result)
+            for idx in range(len(result)):
+                res = result[idx]
+                #print("res",res)
+                for line in res:
+                    # prints each recognised text
+                    print(line)
+                
+            result = result[0]
+            image = Image.open(img_path).convert('RGB')
+            boxes = [line[0] for line in result]
+            txts = [line[1][0] for line in result]
+            scores = [line[1][1] for line in result]
+            im_show = draw_ocr(image, boxes, txts, scores, font_path="./ocr/simfang.ttf")
+            im_show = Image.fromarray(im_show)
+            ocr_path = 'ocr_output/{}_ocr_{}.jpg'.format(
+                image_path.split("/")[-1].split(".")[0], i
+            )
+            im_show.save(ocr_path)
+            print("{} has been saved.".format(ocr_path))
+            
+            
+            # feed ocr results to llava (input: user prompt, ocr output, cropped image)
+            disable_torch_init()
+
+            model_name = get_model_name_from_path(args.model_path)
+            tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, args.model_base, model_name, args.load_8bit, args.load_4bit, device=args.device)
+
+            if 'llama-2' in model_name.lower():
+                conv_mode = "llava_llama_2"
+            elif "v1" in model_name.lower():
+                conv_mode = "llava_v1"
+            elif "mpt" in model_name.lower():
+                conv_mode = "mpt"
+            else:
+                conv_mode = "llava_v0"
+
+            if args.conv_mode is not None and conv_mode != args.conv_mode:
+                print('[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}'.format(conv_mode, args.conv_mode, args.conv_mode))
+            else:
+                args.conv_mode = conv_mode
+
+            conv = conv_templates[args.conv_mode].copy()
+            if "mpt" in model_name.lower():
+                roles = ('user', 'assistant')
+            else:
+                roles = conv.roles
+            # pass cropped image from segmentation
+            print("Opening image: {}".format(img_path))
+            image = Image.open(img_path).convert('RGB')
+            # Similar operation in model_worker.py
+            image_tensor = process_images([image], image_processor, args)
+            if type(image_tensor) is list:
+                image_tensor = [image.to(model.device, dtype=torch.float16) for image in image_tensor]
+            else:
+                image_tensor = image_tensor.to(model.device, dtype=torch.float16)
+
+            while True:
+                try:
+                    if llava_prompt != '':
+                        inp = llava_prompt
+                        llava_prompt = ''
+                    else: 
+                        inp = input(f"{roles[0]}: ")
+                    print("USER: {}".format(inp))
+                except EOFError:
+                    inp = ""
+                if not inp:
+                    print("exit...")
+                    break
+
+                print(f"{roles[1]}: ", end="")
+
+                if image is not None:
+                    # first message
+                    if model.config.mm_use_im_start_end:
+                        inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+                    else:
+                        inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
+                    conv.append_message(conv.roles[0], inp)
+                    image = None
+                else:
+                    # later messages
+                    conv.append_message(conv.roles[0], inp)
+                conv.append_message(conv.roles[1], None)
+                prompt = conv.get_prompt()
+
+                input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+                stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+                keywords = [stop_str]
+                stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+                streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+                with torch.inference_mode():
+                    output_ids = model.generate(
+                        input_ids,
+                        images=image_tensor,
+                        do_sample=True,
+                        temperature=args.temperature,
+                        max_new_tokens=args.max_new_tokens,
+                        streamer=streamer,
+                        use_cache=True,
+                        stopping_criteria=[stopping_criteria])
+
+                outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
+                conv.messages[-1][-1] = outputs
+
+                if args.debug:
+                    print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])
